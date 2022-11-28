90928fe7f961f1f6eba0d9bfb8f76270decd514b
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-28 16:18:59 +0600

    added container in header componennt

---
+++ src/componentns/HOC/Container/container.component.jsx
@@ -0,0 +1,6 @@
+import "./container.style.scss";
+const Container = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
+
+export default Container;
--- src/componentns/header/header.component.jsx
+++ src/componentns/header/header.component.jsx
@@ -1,33 +1,36 @@
 import { FaFacebookF, FaTwitter, FaDribbble } from "react-icons/fa";
+import Container from "../HOC/Container/container.component";
 import "./header.style.scss";
 
 export function HeaderTop() {
   return (
     <div className="header-top">
-      <div className="header-top__login">
-        <a href="">Login</a>
-        <a href="">Register</a>
-      </div>
-      <div className="header-top__icons">
-        <a href="">
-          <FaFacebookF />
-        </a>
-        <a href="">
-          <FaTwitter />
-        </a>
-        <a href="">
-          <FaDribbble />
-        </a>
-      </div>
-      <div className="header-top__cart">
-        <select name="" id="">
-          <option value="">USD</option>
-          <option value="">EUR</option>
-          <option value="">BDT</option>
-        </select>
-        <a href="">Cart</a>
-        <a href="">Cart</a>
-      </div>
+      <Container>
+        <div className="header-top__login">
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </div>
+        <div className="header-top__icons">
+          <a href="">
+            <FaFacebookF />
+          </a>
+          <a href="">
+            <FaTwitter />
+          </a>
+          <a href="">
+            <FaDribbble />
+          </a>
+        </div>
+        <div className="header-top__cart">
+          <select name="" id="">
+            <option value="">USD</option>
+            <option value="">EUR</option>
+            <option value="">BDT</option>
+          </select>
+          <a href="">Cart</a>
+          <a href="">Cart</a>
+        </div>
+      </Container>
     </div>
   );
 }
